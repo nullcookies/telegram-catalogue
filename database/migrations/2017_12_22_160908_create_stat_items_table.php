2e75ca2f79e5aa8bc3b9d152bd22b5d752cb1c9d
Alexander <shkipersany@gmail.com>
2017-12-22 22:48:23 +0200

    Rename migration

--- database/migrations/2017_12_22_160908_create_stat_items_table.php
+++ database/migrations/2017_12_22_160908_create_stat_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStatItemstemsTable extends Migration
+class CreateStatItemsTable extends Migration
 {
     /**
      * Run the migrations.
